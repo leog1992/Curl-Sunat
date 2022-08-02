ff330b3aa65c282d3215a55e28abfae53849a01c
Luis Oyanguren <leog.1992@gmail.com>
2022-08-02 12:41:16 -0500

    Update TestPortal.php

--- TestPortal.php
+++ TestPortal.php
@@ -10,5 +10,9 @@ require_once 'Portal.php';
 
 use Sunat\Portal;
 $portal = new Portal();
-echo "Starting...<br/>";
-$portal->Run();
+//echo "Starting...<br/>";
+$array = $portal->Login("10123456781","USUERIOSOL","CLAVESOL");
+$urlfinal= $array[0];
+
+//abrira el portal sunat sin necesidad de hacer login
+header("Location: " . $urlfinal);
